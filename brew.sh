d692fbef1d66874a1078543b7a0e426895827ab2
Sergei Kim <srg.kim@gmail.com>
2012-08-16 23:40:42 +0400

    Split brew

--- brew.sh
+++ brew.sh
@@ -3,15 +3,4 @@
 # Get brew
 ruby <(curl -fsSk https://raw.github.com/mxcl/homebrew/go)
 
-# Make sure we’re using the latest Homebrew
-brew update
-
-# Upgrade any already-installed formula
-brew upgrade
-
-brew install mysql
-brew install pil
-brew install node
-
-# Remove outdated versions from the cellar
-brew cleanup
+brew install mysql pil node
---
+++ brew_maint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formula
+brew upgrade
+
+# Remove outdated versions from the cellar
+brew cleanup
